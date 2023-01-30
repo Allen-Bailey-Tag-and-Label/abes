1856221053e06fcdd40a063fd4deb3f64e55f3c8
Bob McAleavey <bobmcaleavey@gmail.com>
2023-01-30 13:31:52 -0500

    feat: add default app styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,3 +3,14 @@
 </script>
 
 <slot />
+
+<style>
+  :global(::-webkit-scrollbar) {
+    height: 0.5rem;
+    width: 0.5rem;
+  }
+  :global(::-webkit-scrollbar-thumb) {
+    background: rgb(49, 56, 185);
+    border-radius: 0.5rem;
+  }
+</style>
