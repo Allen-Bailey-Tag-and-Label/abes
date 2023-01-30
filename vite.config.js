e1ffe385206455eff64108bd8680425849f6441a
Bob McAleavey <bobmcaleavey@gmail.com>
2023-01-30 13:22:23 -0500

    feat: add path aliases

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { resolve } from 'path';
 
 const config = {
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      $auth: resolve('./src/auth'),
+      $components: resolve('./src/components'),
+      $db: resolve('./src/db'),
+      $icons: resolve('./src/icons'),
+      $lib: resolve('./src/lib'),
+      $routes: resolve('./src/routes'),
+      $src: resolve('./src')
+    }
+  }
 };
 
 export default config;
